27578465544a07c7c78de37df9a4d120d020076f
lialvin <li_alvin@qq.com>
2018-11-05 16:29:55 +0800

    1

--- src/table/tableframe.h
+++ src/table/tableframe.h
@@ -2,13 +2,15 @@
 #define TABLEFRAME_H
 
 #include <QFrame>
+#include <QMap>
 #include <memory>
+
+#include "codebase/utility/httpclient.h"
 
 namespace Ui {
 class TableFrame;
 }
 
-class HttpClient;
 class QTextEdit;
 
 class TableFrame : public QFrame
@@ -16,18 +18,24 @@ class TableFrame : public QFrame
     Q_OBJECT
 
 public:
-    explicit TableFrame(QWidget *parent = 0);
+    explicit TableFrame(QWidget *parent = nullptr);
     ~TableFrame();
 
 private:
     void formatPrint(const QByteArray& data, QTextEdit *edit);
 
+    void updateTableList();
+    void initHttpClients();
+
 private slots:
     void on_pushButtonGetTable_clicked();
 
+    void on_pushButtonGetStruct_clicked();
+
 private:
     Ui::TableFrame              *ui;
-    std::shared_ptr<HttpClient> httpc;
+    QMap<FunctionID, std::shared_ptr<HttpClient>> httpcs;
+    QByteArray contractAbi;
 };
 
 #endif // TABLEFRAME_H
