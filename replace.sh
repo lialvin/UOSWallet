56fade8a6de5c393df933d3ea4a229fe8c6f47d9
lialvin <li_alvin@qq.com>
2018-11-05 16:58:34 +0800

    1

--- replace.sh
+++ replace.sh
@@ -1,6 +1,6 @@
 # .bashrc
 find . -type f -print0 | xargs -0 sed -i 's/EOS/UOS/g'
-ulord@host-10-186-11-220:~/EOSDevHelper$ find . -type f -print0 | xargs -0 sed -i 's/Eos/Uos/g'
-ulord@host-10-186-11-220:~/EOSDevHelper$ find . -type f -print0 | xargs -0 sed -i 's/uos/uos/g'
-ulord@host-10-186-11-220:~/EOSDevHelper$ find . -exec rename 's/EOS/UOS/' {} ";"
-ulord@host-10-186-11-220:~/EOSDevHelper$ find . -exec rename 's/eos/uos/' {} ";"
+find . -type f -print0 | xargs -0 sed -i 's/Eos/Uos/g'
+find . -type f -print0 | xargs -0 sed -i 's/uos/uos/g'
+find . -exec rename 's/EOS/UOS/' {} ";"
+find . -exec rename 's/eos/uos/' {} ";"
